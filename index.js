c115cbd6266d811a61e67fda83cc01de189298c6
Evangeline VIllena <vvanjie@yahoo.com>
2018-01-29 03:52:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ add (1, 80)
 function subtract (a, b){
   return a - b
 } 
-add (60, 40)
+subtract (60, 40)
 
+function subtract (a, b){
+  return a - b
+} 
+subtract (60, 40)
+
+function subtract (a, b){
+  return a - b
+} 
+subtract (60, 40)
 
 60 - 40 // 20
 2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
