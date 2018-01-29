96759e87663c240cc12508def200df9630b4a768
Evangeline VIllena <vvanjie@yahoo.com>
2018-01-29 03:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ function add (a, b){
 } 
 add (1, 80)
 
+function add (a, b){
+  return a + b // 81
+} 
+add (1, 80)
+
+
 60 - 40 // 20
 2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
 5.0 / 2.5 // 2
