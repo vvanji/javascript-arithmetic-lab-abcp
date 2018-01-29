f14decffd589a1f5d1ecdc872ae92de996878636
Evangeline VIllena <vvanjie@yahoo.com>
2018-01-29 03:49:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function add (a, b){
-  return = a + b // 81
+  return a + b // 81
 } 
-add ()
+add (1, 80)
+
 60 - 40 // 20
 2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
 5.0 / 2.5 // 2
